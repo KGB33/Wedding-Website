6ef8204d60b4842006cf073e3c5a47ea761e8ada
KGB33 <33pigbass@gmail.com>
2019-08-21 12:26:38 -0500

    added profile blueprint/routes

---
+++ flaskr/profile.py
@@ -0,0 +1,23 @@
+from flask import Blueprint
+from flask_login import login_required
+
+bp = Blueprint('profile', __name__, url_prefix='/profile')
+
+
+@bp.route('/view')
+@login_required
+def view():
+    """
+    Route for viewing profile info
+    """
+    return 'This is the profile view'
+
+
+@bp.route('/edit')
+@login_required
+def edit():
+    """
+    Route for editing profile info
+    """
+    return 'This is the edit profile view'
+
