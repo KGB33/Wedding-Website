458cd6e879be527b84b274a7fdaf45675ded33e7
KGB33 <33pigbass@gmail.com>
2019-08-26 20:29:26 -0500

    testing config

---
+++ tests/flask_test.py
@@ -0,0 +1,14 @@
+from WeddingWebsite.secrets import DB_HOST_TEST
+
+# Bcrypt algorithm hashing rounds (reduced for testing purposes only!)
+BCRYPT_LOG_ROUNDS = 4
+
+# Enable the TESTING flag to disable the error catching during request handling
+# so that you get better error reports when performing test requests against the application.
+TESTING = True
+
+# Disable CSRF tokens in the Forms (only valid for testing purposes!)
+WTF_CSRF_ENABLED = False
+
+# Connect to the test DB
+MONGO_URI = DB_HOST_TEST
