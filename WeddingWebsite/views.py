e4d0ee45d57e828584ccc046969bbc85cd0c0536
KGB33 <33pigbass@gmail.com>
2019-08-28 17:03:12 -0500

    added home page and its html/css

--- WeddingWebsite/views.py
+++ WeddingWebsite/views.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, redirect, render_template, url_for
 from flask_login import current_user, fresh_login_required, login_required
+
+from WeddingWebsite.extensions import mongo
+from WeddingWebsite.forms import EditForm
+from WeddingWebsite.models import GuestCollection
 
 
 views = Blueprint("views", __name__, url_prefix="")
@@ -7,12 +11,15 @@ views = Blueprint("views", __name__, url_prefix="")
 
 @views.route("/")
 def index():
+    if current_user.is_authenticated:
+        return redirect(url_for("views.home"))
     return render_template("index.html")
 
 
-@views.route("/member")
-def member_page():
-    return "This is the members only page"
+@views.route("/home")
+@login_required
+def home():
+    return render_template("home.html")
 
 
 @views.route("/view_profile")
@@ -27,7 +34,17 @@ def view_profile():
 @views.route("/edit_profile")
 @fresh_login_required
 def edit_profile():
-    """
-    Route for editing profile info
-    """
-    return "This is the edit profile view"
+    # TODO: add tests
+    guest = GuestCollection().get_guest_by_id(current_user.id)
+    form = EditForm()
+    if form.validate_on_submit():
+        if form.username.data:
+            guest.username = form.username.data
+        if form.password.data:
+            guest.password = form.password.data
+        if form.name.data:
+            guest.name = form.name.data
+        if form.email.data:
+            guest.email = form.email.data
+        guest.update_db(mongo.db)
+        flash("Changes Saved")
