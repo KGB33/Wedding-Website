1744af456c709350bb416e40507fdf734f956c3b
KGB33 <33pigbass@gmail.com>
2019-09-03 10:16:27 -0500

    added tests for the admin BP

---
+++ tests/test_admin/test_dashbord.py
@@ -0,0 +1,68 @@
+from tests.conftest import log_in
+
+
+class TestDashboardAccess:
+    def test_anon_user(self, test_client):
+        """
+        GIVEN a flask app
+        WHEN an anon user requests '/admin'
+        THEN check that they are redirected to the sign in page
+        """
+        response = test_client.get("/admin", follow_redirects=True)
+        assert response.status_code == 200
+        assert b"Sign In" in response.data
+
+    def test_auth_user_without_admin_role(self, test_client):
+        """
+        GIVEN a flask app
+        WHEN an authorized user w/o the admin role requests '/admin'
+        THEN check that they are redirected to '/auth/unauthorized_role'
+        """
+        assert log_in(test_client)
+        response = test_client.get("/admin", follow_redirects=True)
+        assert response.status_code == 200
+        assert (
+            b"you lack the required roles to view your requested page." in response.data
+        )
+
+    def test_auth_user_with_admin_role(self, test_client):
+        """
+        GIVEN a flask app
+        WHEN an authorized user w/ the admin role requests '/admin'
+        THEN check that the page is rendered
+        """
+        assert log_in(test_client, username="t_admin")
+        response = test_client.get("/admin", follow_redirects=True)
+        assert response.status_code == 200
+        assert b"<h1>Guests:</h1>" in response.data
+        assert b"User t_admin," in response.data
+
+
+class TestDashboardView:
+    def test_template_render(self, test_client):
+        """
+        GIVEN a flask app
+        WHEN the '/admin page is requested by the appropriate user
+        THEN check that the template displays all registered users appropriately
+        """
+        assert log_in(test_client, username="t_admin")
+        response = test_client.get("/admin", follow_redirects=True)
+        assert response.status_code == 200
+        assert b"<h1>Guests:</h1>" in response.data
+        assert b"User t_admin," in response.data
+        assert b"User t_groomsman" in response.data
+        assert b"User t_bridesmaid" in response.data
+        assert b"User t_wedding_party" in response.data
+        assert b"User t_default" in response.data
+
+    def test_admin_and_admin_guests(self, test_client):
+        """
+        GIVEN a flask app
+        CHECK that the routes '/admin' and '/admin/guest' return the same content
+        """
+        assert log_in(test_client, username="t_admin")
+        response_1 = test_client.get("/admin", follow_redirects=True)
+        response_2 = test_client.get("/admin/guests", follow_redirects=True)
+        assert response_1.status_code == 200
+        assert response_2.status_code == 200
+        assert response_1.data == response_2.data
---
+++ tests/test_admin/test_guest_id.py
@@ -0,0 +1,21 @@
+from tests.conftest import log_in
+
+
+def get_guest_id(db, username="t_default"):
+    return db.guests.find_one({"username": username})["_id"]
+
+
+def test_admin_guest_view_default_guest(test_client, mongo_db):
+    """
+    GIVEN a flask app
+    WHEN '/auth/guests/<guest_id>' is requested with 't_default's ID
+    THEN check that 't_default's data is displayed
+    """
+    assert log_in(test_client, username="t_admin")
+    response = test_client.get(f"/admin/guest/{get_guest_id(mongo_db)}")
+    assert response.status_code == 200
+    print(response.data)
+    assert b"<title> t_default </title>" in response.data
+    assert b"<li>Name: t_default</li>" in response.data
+    assert b"<li>Email: td@test.org</li>" in response.data
+    assert b"<li>Roles:\n            \n                None" in response.data
