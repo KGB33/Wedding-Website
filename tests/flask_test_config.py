d5a22f7364489fc930ed2e3f17f8d5c2ef87ed59
KGB33 <33pigbass@gmail.com>
2019-10-04 12:14:35 -0500

    replaced by config.py

--- tests/flask_test_config.py
+++
@@ -1,9 +0,0 @@
-# Enable the TESTING flag to disable the error catching during request handling
-# so that you get better error reports when performing test requests against the application.
-TESTING = True
-
-# Disable CSRF tokens in the Forms (only valid for testing purposes!)
-WTF_CSRF_ENABLED = False
-
-# Connect to the test DB
-MONGO_URI = "mongodb://localhost"
