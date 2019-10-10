df876fcd83af259c125021959d1f6ec897093c38
KGB33 <33pigbass@gmail.com>
2019-10-09 22:56:41 -0500

    added tests for building the mail messages

--- tests/test_mail/test_build_mail.py
+++ tests/test_mail/test_build_mail.py
@@ -1,10 +1,12 @@
-import pytest
-
-from WeddingWebsite.mail import build_rsvp_email, Message, Recipient
+from WeddingWebsite.mail import build_rsvp_email, get_recipients, Message, Recipient
 from WeddingWebsite.models import GuestCollection
 
 
 class TestSendMail:
+    """
+    Currently Unable to mock API call, not testing ATM
+    """
+
     pass
 
 
@@ -25,9 +27,40 @@ class TestBuildRsvpEmail:
 
 
 class TestBuildCustomEmail:
-    @pytest.fixture(params=[])
-    def expected_custom(self):
-        pass
+    """
+    Same as the Messages Constructor
+    """
 
-    def test_build_send_to_all(self):
-        pass
+    pass
+
+
+class TestGetRecipients:
+    def test_send_to_all(self, mongo_db):
+        recipients = get_recipients(send_to_all=True)
+        expected = [
+            Recipient(guest.email, guest.name)
+            for guest in GuestCollection(mongo_db.guests)
+        ]
+        assert set(recipients) == set(expected)
+
+    def test_require_all_roles(self, mongo_db, template_user):
+        roles = ["cabin_stayer", "a made up role", "TesterJester"]
+        template_user.roles = roles
+        template_user.add_to_collection(mongo_db.guests)
+        recipients = get_recipients(require_any_all_roles="all", recipient_roles=roles)
+        expected = [Recipient(template_user.email, template_user.name)]
+        assert expected == recipients
+
+    def test_require_any_roles(self, template_user, mongo_db):
+        roles = ["cabin_stayer", "a made up role", "TesterJester", "groomsman"]
+        template_user.roles = roles
+        template_user.add_to_collection(mongo_db.guests)
+
+        recipients = get_recipients(require_any_all_roles="any", recipient_roles=roles)
+        expected = [
+            Recipient(template_user.email, template_user.name),
+            Recipient("tg@test.org", "t_groomsman"),
+        ]
+
+        # Use sets to test regardless of order
+        assert set(expected) == set(recipients)
