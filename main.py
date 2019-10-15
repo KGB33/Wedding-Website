1e2301dd43da57d81a2eedb716fc5387390ad507
KGB33 <33pigbass@gmail.com>
2019-10-15 17:30:39 -0500

    fixed error where GAE could not start the app

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from WeddingWebsite import create_app
 from WeddingWebsite.config import DevelopmentConfig
 
+app = create_app(DevelopmentConfig)
+
 if __name__ == "__main__":
-    app = create_app(DevelopmentConfig)
     app.run()
