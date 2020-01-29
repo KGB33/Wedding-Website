672fb8cd2c5db2d9f5c32bb54b5385e2b245589a
KGB33 <33pigbass@gmail.com>
2020-01-29 16:24:05 -0600

    swaped from .py to .env

--- WeddingWebsite/config.py
+++ WeddingWebsite/config.py
@@ -1,8 +1,17 @@
 import os
 
 
-if not (DB_HOST := os.getenv("DB_HOST")):
-    raise OSError("DB_HOST URL not in .env")
+if not (DB_USER := os.getenv("DB_USER")):
+    raise OSError("DB_USER not in .env")
+
+if not (DB_PASSWORD := os.getenv("DB_PASSWORD")):
+    raise OSError("DB_PASSWORD not in .env")
+
+if not (DB_NAME := os.getenv("DB_NAME")):
+    raise OSError("DB_NAME not in .env")
+
+if not (DB_NAMESPACE := os.getenv("DB_NAMESPACE")):
+    raise OSError("DB_NAMESPACE not in .env")
 
 if not (FLASK_SECRET_KEY := os.getenv("FLASK_SECRET_KEY")):
     raise OSError("FLASK_SECRET_KEY not in .env")
@@ -22,5 +31,5 @@ class TestingConfig(BaseConfig):
     MONGO_URI = "mongodb://127.0.0.1:27017/test"
 
 
-class DevelopmentConfig(BaseConfig):
-    MONGO_URI = DB_HOST
+class ProductionConfig(BaseConfig):
+    MONGO_URI  = f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_NAME}-pylxy.mongodb.net/{DB_NAMESPACE}?retryWrites=true&w=majority"
