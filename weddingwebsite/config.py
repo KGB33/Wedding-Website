b60279115e83eb5cdb05278b9a0d0b51304e7d14
kgb33 <github@bassingthwaite.org>
2020-03-29 18:50:02 -0500

    updated handleing of ENV vars

--- weddingwebsite/config.py
+++ weddingwebsite/config.py
@@ -5,6 +5,7 @@ if not (FLASK_SECRET_KEY := os.getenv("FLASK_SECRET_KEY")):
     raise OSError("FLASK_SECRET_KEY not in .env")
 
 
+
 class BaseConfig:
     SECRET_KEY = FLASK_SECRET_KEY
     USER_APP_NAME = "Wedding-Website"  # Shown in and email templates and page footers
@@ -20,12 +21,9 @@ class TestingConfig(BaseConfig):
 
 
 class ProductionConfig(BaseConfig):
-    if not (DB_USER := os.getenv("DB_USER")):
-        raise OSError("DB_USER not in .env")
-    if not (DB_PASSWORD := os.getenv("DB_PASSWORD")):
-        raise OSError("DB_PASSWORD not in .env")
-    if not (DB_NAME := os.getenv("DB_NAME")):
-        raise OSError("DB_NAME not in .env")
-    if not (DB_NAMESPACE := os.getenv("DB_NAMESPACE")):
-        raise OSError("DB_NAMESPACE not in .env")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_NAMESPACE = os.getenv("DB_NAMESPACE")
     MONGO_URI = f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_NAME}-pylxy.mongodb.net/{DB_NAMESPACE}?retryWrites=true&w=majority"
+    if "None" in MONGO_URI: raise EnvironmentError("Database info not in ENV")
