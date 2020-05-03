f0f32d3f7c855865af28dcb12d0d7d1b78333b6b
GitHub Action <action@github.com>
2020-05-03 21:48:14 +0000

    ci: Auto-Formatted with Black

--- weddingwebsite/exceptions.py
+++ weddingwebsite/exceptions.py
@@ -1,8 +1,10 @@
 class NoContentProvided(TypeError):
     """ Raised when No Content is provided for an email. """
+
     pass
 
 
 class TooManyMembersError(Exception):
     """ Raised when a member is added to a full group """
+
     pass
